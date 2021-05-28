b5c5f6a02d7b9c992450c850cf60d6cd70d123e8
baptiste <baptiste.bordet@gmail.com>
2021-05-28 17:56:04 +0200

    add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      darkGray: '#1c1e27',
+      white: '#FFFFFF'
+    },
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        sm: '4rem',
+        lg: '20%',
+        xl: '20%',
+        '2xl': '20%',
+      }
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#3490dc',
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+    }),
     extend: {},
   },
   variants: {
